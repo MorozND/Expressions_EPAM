7facac5e45dbb647bb8cc227071ea5d5ee29a522
Mikita Maroz <nikitamoroz118@gmail.com>
2022-07-07 15:45:00 +0200

    Add Increment/Decrement expression convertion

--- ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
+++ ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -1,9 +1,69 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
     public class IncDecExpressionVisitor : ExpressionVisitor
     {
-        // todo: feel free to add your code here
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            Expression resultExpression = null;
+
+            switch (node.NodeType)
+            {
+                case ExpressionType.Add:
+                    TryConvertBinaryExpression(node, ExpressionConvertTypes.Increment, out resultExpression);
+                    break;
+
+                case ExpressionType.Subtract:
+                    TryConvertBinaryExpression(node, ExpressionConvertTypes.Decrement, out resultExpression);
+                    break;
+            }
+            
+            return resultExpression ?? base.VisitBinary(node);
+        }
+
+        private bool TryConvertBinaryExpression(BinaryExpression expression, ExpressionConvertTypes convertType, out Expression resultExpression)
+        {
+            Expression argumentExpression = null;
+            ConstantExpression constantExpression = null;
+
+            if (expression.Left.NodeType == ExpressionType.Constant)
+            {
+                constantExpression = expression.Left as ConstantExpression;
+                argumentExpression = expression.Right;
+            }
+            else if (expression.Right.NodeType == ExpressionType.Constant)
+            {
+                constantExpression = expression.Right as ConstantExpression;
+                argumentExpression = expression.Left;
+            }
+            else
+            {
+                resultExpression = null;
+                return false;
+            }
+
+            if (unchecked((int)constantExpression.Value) == 1)
+            {
+                switch (convertType)
+                {
+                    case ExpressionConvertTypes.Increment:
+                        resultExpression = Expression.Increment(argumentExpression);
+                        return true;
+
+                    case ExpressionConvertTypes.Decrement:
+                        resultExpression = Expression.Decrement(argumentExpression);
+                        return true;
+
+                    default:
+                        resultExpression = null;
+                        return false;
+                }
+            }
+
+            resultExpression = null;
+            return false;
+        }
     }
 }
